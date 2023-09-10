498beaa47cbfd47e229f2d5544a4b3b0510bf54b
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2023-09-10 19:21:33 -0300

    finalizado recurso de notificações

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -39,6 +39,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.4);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -66,8 +67,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -75,9 +78,6 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${(props) =>
@@ -90,6 +90,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
