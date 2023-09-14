ac3312558d27b49eab9179ce9be8f133d35a12a6
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2023-09-13 23:42:37 -0300

    atualização de perfil

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,6 +1,19 @@
-export function updateProfileRequest(oldPassword, newPassword) {
+export function updateProfileRequest(data) {
   return {
     type: '@user/UPDATE_PROFILE_REQUEST',
-    payload: { oldPassword, newPassword },
+    payload: { data },
+  };
+}
+
+export function updateProfileSuccess(profile) {
+  return {
+    type: '@user/UPDATE_PROFILE_SUCCESS',
+    payload: { profile },
+  };
+}
+
+export function updateProfileFailure() {
+  return {
+    type: '@user/UPDATE_PROFILE_FAILURE',
   };
 }
