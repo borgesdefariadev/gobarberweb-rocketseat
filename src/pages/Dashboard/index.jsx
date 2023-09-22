f4c1adaabef4653ac2ebb5b3069bb565faec77d5
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2023-09-21 22:08:32 -0300

    estilização inicial da dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import api from '../../services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 function Dashboard() {
-  api.get('appointments');
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>21 de Setembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
 
-  return <h1>DASHBOARD</h1>;
+      <ul>
+        <Time>
+          <strong>08:00</strong>
+          <span>Ricardo</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Horário disponível</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>João</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      border: 0;
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+`;
+
+export const Time = styled.ul`
+  display: flex;
+`;
